e8eb968b02cc2661028dfcb8d7c73e261e52f568
Gaurav-Nathh <gauravnath725@gmail.com>
2025-04-18 13:18:31 +0530

    Added route for the shopping-cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/login/login.component').then((c) => c.LoginComponent),
   },
+  {
+    path: 'shopping-cart',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/shopping-cart/shopping-cart.component').then(
+        (c) => c.ShoppingCartComponent
+      ),
+  },
 ];
