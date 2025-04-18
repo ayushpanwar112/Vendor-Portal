64dba881f55190d83b32d81570b0ed42c7e8f2d8
ayush <panwarkanu2001@gmail.com>
2025-04-18 16:00:43 +0530

    resolve conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,21 @@ import { Routes } from '@angular/router';
 import { ItemMappingComponent } from './pages/item-mapping/item-mapping.component';
 import { UpdatePasswordComponent } from './pages/update-password/update-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'shopping-cart',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/shopping-cart/shopping-cart.component').then(
+        (c) => c.ShoppingCartComponent
+      ),
+  },
+    {path:'item-mapping',component:ItemMappingComponent},
+    {path:'change-password',component:UpdatePasswordComponent},
+];
